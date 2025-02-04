8c87daa808d4b4b3f8d6ff6c313d1c094e781cfc
JonasBachmeier-UNI <Jonas.Bachmeier@stud.uni-regensburg.de>
2025-02-04 21:10:42 +0100

    Improve Button texture usage + add options menu

--- nohonoramongthieves/scripts/StartMenu.cs
+++ nohonoramongthieves/scripts/StartMenu.cs
@@ -4,19 +4,23 @@ using System.Runtime.CompilerServices;
 
 public partial class StartMenu : Control
 {
+    private PackedScene _optionsMenuScene;
+
     private Button _startButton;
     private Button _optionsButton;
     private Button _quitButton;
 
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
+        _optionsMenuScene = GD.Load<PackedScene>("res://scenes/options_menu.tscn");
+
         _startButton = GetNode<Button>("StartButton");
         _optionsButton = GetNode<Button>("OptionsButton");
         _quitButton = GetNode<Button>("QuitButton");
-		
+
         _startButton.Pressed += _OnStartButtonPressed;
         _optionsButton.Pressed += _OnOptionsButtonPressed;
-		_quitButton.Pressed += _OnQuitButtonPressed;
+        _quitButton.Pressed += _OnQuitButtonPressed;
     }
 
     private void _OnStartButtonPressed()
@@ -28,9 +32,10 @@ public partial class StartMenu : Control
     {
         GD.Print("Options button pressed");
         Node current_scene = GetTree().CurrentScene;
-        Node options_scene = GD.Load<Node>("res://scenes/options_menu.tscn");
+        
+        Control optionsMenuInstance = _optionsMenuScene.Instantiate<Control>();
 
-        current_scene.AddChild(options_scene);
+        current_scene.AddChild(optionsMenuInstance);
     }
 
     private void _OnQuitButtonPressed()
