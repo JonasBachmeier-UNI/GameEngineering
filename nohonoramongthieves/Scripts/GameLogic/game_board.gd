9aabcbf3993f6b1755fdac85c6bb234932a186a5
Georg Dechant <86664255+GixD42@users.noreply.github.com>
2025-03-16 14:32:20 +0100

    Made Grid
    Testszene mit 2 Skripten

---
+++ nohonoramongthieves/Scripts/GameLogic/game_board.gd
@@ -0,0 +1,94 @@
+extends Node
+
+@export var tml: TileMapLayer
+
+## für Übersetzung des TMLs in eine Matrix
+var MINIMUM_OFFSET_X: int
+var MINIMUM_OFFSET_Y: int
+
+@export var movement_cost = {
+	0 : 1,
+	1 : 999
+}
+
+## Dimensionen der Matrix
+var width: int
+var height: int
+## Matrix für TML
+var grid = []
+signal matrix_ready(value)
+
+func _ready() -> void:
+	var used_cells = tml.get_used_cells()
+	calculate_minimum_offsets(used_cells)
+	
+	create_matrix(used_cells)
+	emit_signal("matrix_ready", grid)
+	
+func _process(delta: float) -> void:
+	pass
+
+
+func calculate_minimum_offsets(cells: Array[Vector2i]):
+	var current_min_x = 0
+	var current_min_y = 0
+	var current_max_x = 0
+	var current_max_y = 0
+	
+	for cell in cells:
+		if current_min_x > cell[0]:
+			current_min_x = cell[0]
+		if current_min_y > cell[1]:
+			current_min_y = cell[1]
+			
+		if current_max_x < cell[0]:
+			current_max_x = cell[0]
+		if current_max_y < cell[1]:
+			current_max_y = cell[1]
+			
+	MINIMUM_OFFSET_X = abs(current_min_x)
+	MINIMUM_OFFSET_Y = abs(current_min_y)
+	
+	width = current_max_x - current_min_x + 1
+	height = current_max_y - current_min_y + 1
+	
+	
+func create_matrix(cells: Array[Vector2i]):
+	
+	for x in range(height):
+		grid.append([])
+		for y in range(width):
+			grid[x].append(1000)
+	
+	# Matrix Koordinaten normalisiert und mit Atlas-ID befüllt
+	for cell in cells:
+		var grid_coords = tml_to_grid_coords(cell)
+		var value = tml.get_cell_source_id(cell)
+		set_grid_value(grid_coords, movement_cost[value])
+		
+	
+
+func tml_to_grid_coords(tml_coords: Vector2i):
+	return Vector2i(
+		tml_coords[0] + MINIMUM_OFFSET_X,
+		tml_coords[1] + MINIMUM_OFFSET_Y
+		)
+		
+func grid_to_tml_coords(grid_coords: Vector2i):
+	return Vector2i(
+		grid_coords[0] + MINIMUM_OFFSET_X,
+		grid_coords[1] + MINIMUM_OFFSET_Y
+		)
+
+func get_grid_value(coords: Vector2i):
+	return grid[coords[1]][coords[0]]
+	
+func set_grid_value(coords: Vector2i, value):
+	grid[coords[1]][coords[0]] = value
+
+func print_grid():
+	for x in grid:
+		print(x)
+		
+func get_grid():
+	return grid
---
+++ nohonoramongthieves/Scripts/GameLogic/unit.gd
@@ -0,0 +1,238 @@
+extends Path2D
+
+const ID_CONVERT_MULT = 100
+const ENEMY_POSITION_VALUE = 500
+
+## TODO: wenn man gegangen ist kann man nix mehr machen
+
+## Teamzuweisung
+@export var isEnemy: bool
+## Geschwindigkeit auf Pfad
+@export var speed := 300.0
+
+@export var x_coord: int
+@export var y_coord: int
+
+var grid
+var astar = AStar2D.new()
+
+## Gameplay 
+@export var hp:= 10
+@export var dmg := 3
+@export var defense := 1
+# Reichweite der Einheit
+@export var move_range := 2
+
+# Tiles, die erreicht werden können
+# Startfeld drin?
+var in_move_range = []
+# Tiles, die angegriffen werden können
+var in_attack_range = []
+
+func _ready() -> void:
+	pass
+	
+
+
+func _on_game_board_matrix_ready(value: Variant) -> void:
+	grid = value
+	## Koordinaten der Gegner mit ENEMY_POSITION_VALUE belegen
+	print_grid()
+	astar = create_astar_for_grid()
+	print(astar.get_point_path(101, 505))
+	
+
+func get_cells_in_range(cur_x, cur_y):
+	
+	# überprüfen, wie weit man gehen kann
+	var move_count = 0
+	var check_next = [[[cur_x, cur_y]]]
+	
+	in_move_range.append([cur_x, cur_y])
+	# schon geprüfte Felder
+	var checked = []
+	
+	while move_count <= move_range:
+		
+		# Alle im nächsten Zug erreichbare Felder
+		var next_batch = []
+		
+		for element in check_next[move_count]:
+
+			var x_check = element[0]
+			var y_check = element[1]
+			
+			checked.append([x_check, y_check])
+		
+			var temp = get_possible_moves(x_check, y_check, move_count)
+			var reachable_temp = temp[0]
+			var attackable_temp = temp[1]
+		
+			for c in reachable_temp:
+				if !checked.has(c):
+					in_move_range.append(c)
+					next_batch.append(c)
+		
+			for c in attackable_temp:
+				if !checked.has(c) and !in_move_range.has(c):
+					in_attack_range.append(c)
+		
+		check_next.append(next_batch)
+		move_count += 1
+
+
+#	## Testing
+#	for c in in_move_range:
+#		set_grid_value(c[0], c[1], 2)
+#
+#		
+#	for c in in_attack_range:
+#		set_grid_value(c[0], c[1], 3)
+#
+#		
+#	set_grid_value(cur_x, cur_y, 0)
+#		
+#	for x in grid:
+#		print(x)
+	
+
+func get_possible_moves(x, y, move_count):
+	
+	var reachable_neighbours = []
+	var attackable_neighbours = []
+	
+	if move_count > move_range:
+		return [reachable_neighbours, attackable_neighbours]
+
+
+	var max_y = len(grid)
+	var max_x = len(grid[0])
+
+	## Ein Feld unter dem untersuchten existiert
+	if y > 0:
+		## Das untere Feld ist erreichbar
+		var grid_value = get_grid_value(x, y-1)
+		if (grid_value + move_count) <= move_range:
+			reachable_neighbours.append([x, y-1])
+		## Das untere Feld ist nicht erreichbar, aber keine Wand
+		# Enemy = 500, Wand = 999, Loch = 1000
+		elif grid_value < 999:
+			attackable_neighbours.append([x, y-1])
+		
+	## Ein Feld über dem untersuchten existiert
+	if y < max_y-1:
+		## Das obere Feld ist erreichbar
+		var grid_value = get_grid_value(x, y+1)
+		if (grid_value + move_count) <= move_range:
+			reachable_neighbours.append([x, y+1])
+		## Das obere Feld ist nicht erreichbar, aber keine Wand
+		# Enemy = 500, Wand = 999, Loch = 1000
+		elif grid_value < 999:
+			attackable_neighbours.append([x, y+1])
+	
+	## Ein Feld links neben dem untersuchten existiert
+	if x > 0:
+		var grid_value = get_grid_value(x-1, y)
+		## Das linke Feld ist erreichbar
+		if (grid_value + move_count) <= move_range:
+			reachable_neighbours.append([x-1, y])
+		## Das linke Feld ist nicht erreichbar, aber keine Wand
+		# Enemy = 500, Wand = 999, Loch = 1000
+		elif grid_value < 999:
+			attackable_neighbours.append([x-1, y])
+	
+	## Ein Feld links neben dem untersuchten existiert
+	if x < max_x - 1:
+		## Das rechte Feld ist erreichbar
+		var grid_value = get_grid_value(x+1, y)
+		if (grid_value + move_count) <= move_range:
+			reachable_neighbours.append([x+1, y])
+		## Das rechte Feld ist nicht erreichbar, aber keine Wand
+		# Enemy = 500, Wand = 999, Loch = 1000
+		elif grid_value < 999:
+			attackable_neighbours.append([x+1, y])
+
+	return [reachable_neighbours, attackable_neighbours]
+	
+
+func get_path_to_destination(end_x: int, end_y: int):
+	
+	var destination = [end_x, end_y]
+	
+	# nicht erreichbar
+	if !in_attack_range.has(destination) and !in_move_range.has(destination):
+		return []
+	
+	# Wenn nur durch Attacke erreichbar
+	if in_attack_range.has(destination):
+		# kürzesten Pfad zu Nachbar
+		return []
+		
+	# in movement range
+	var id_start = coordinate_to_id(x_coord, y_coord)
+	var id_end = coordinate_to_id(end_x, end_y)
+	var id_path = astar.get_point_path(id_start, id_end)
+	var coord_path = []
+	for id in id_path:
+		coord_path.append(id_to_coordinate(id))
+	return coord_path
+
+
+
+func get_grid_value(x, y):
+	return grid[y][x]
+	
+func set_grid_value(x, y, value):
+	grid[y][x] = value
+	
+func print_grid():
+	for x in grid:
+		print(x)
+
+
+func create_astar_for_grid():
+	
+	## Hier Gegner und Verbündetenposition ins Grid eintragen
+	
+	var y_max = len(grid)
+	var x_max = len(grid[0])
+	
+	## Punkte werden mit Gewichtung auf a* gelegt
+	for y in range(y_max):
+		for x in range(x_max):
+			var id = coordinate_to_id(x, y)
+			astar.add_point(id, Vector2(x, y), get_grid_value(x, y))
+	
+	## Verbindungen zwischen Punkten werden gemacht
+	for y in range(y_max):
+		for x in range(x_max):
+			var id = coordinate_to_id(x, y)
+			
+			if y > 0:
+				var id_n = coordinate_to_id(x, y-1)
+				astar.connect_points(id, id_n)
+				
+			if y < y_max-1:
+				var id_n = coordinate_to_id(x, y+1)
+				astar.connect_points(id, id_n)
+				
+			if x > 0:
+				var id_n = coordinate_to_id(x-1, y)
+				astar.connect_points(id, id_n)
+
+			if x < x_max+1:
+				var id_n = coordinate_to_id(x+1, y)
+				astar.connect_points(id, id_n)
+				
+	return astar
+
+
+	
+func coordinate_to_id(x, y):
+	return x*ID_CONVERT_MULT + y
+	
+func id_to_coordinate(coord):
+	
+	var y = coord % ID_CONVERT_MULT
+	var x = (coord - y) / ID_CONVERT_MULT
+	return [x, y]
